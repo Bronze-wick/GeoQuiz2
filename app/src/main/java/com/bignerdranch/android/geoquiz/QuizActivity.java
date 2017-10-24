0cc51b565fbef4c5f61e10c28788d7d36af04680
jpata000 <bronzewick.llc@gmail.com>
2017-10-24 15:38:53 -0400

    James Pata commit project 2 redo after replaced PC

--- app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -7,7 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-//GeoQuiz2.1.0
+//GeoQuiz3.0.0
 
 public class QuizActivity extends AppCompatActivity {
 
